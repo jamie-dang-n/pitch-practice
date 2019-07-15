dfe45d9a6885a4cad41703467248bf4b88e5af29
gothhouse <jamiedang1349@gmail.com>
2019-07-15 13:32:47 -0700

    first

---
+++ pitch-practice/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  pitch-practice
+//
+//  Created by Apple on 7/12/19.
+//  Copyright © 2019 Anika. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+
+
+//pitchnotes
+
+// v1: artist social media
+
+//v2: Gesture drawing app
+
+//v3: game -- vis nov/dating sim
+
+//v4: RPG
+
+//v5: simple cat game(think: neko atsume/tomagachi)
